f63c6f326f467d08f180b0cc375b39ed9dbe4806
kasheemlew <417879751@qq.com>
2018-10-26 23:56:34 +0800

    add kNN

---
+++ kNN/kNN.py
@@ -0,0 +1,32 @@
+import operator
+import numpy as np
+
+def createDataSet():
+    group = np.array([
+        [1.0, 1.1],
+        [1.0, 1.0],
+        [0, 0],
+        [0, 0.1]
+    ])
+    labels = ['A', 'A', 'B', 'B']
+    return groups, labels
+
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(
+        classCount.iteritems(),
+        key=operator.itemgetter(1),
+        revrese=True
+    )
+    return sortedClassCount[0][0]
+
--- stock-price-predict/main.py
+++ stock-price-predict/main.py
@@ -3,15 +3,17 @@ import math
 import quandl
 import pandas as pd
 
-from sklearn import preprocessing, cross_validation, svm
-from sklearn.linear_model import LinearRegression
-
+quandl.ApiConfig.api_key = 'f4szRnCwN8uN3zPKF23T'
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
-print(df.head())
+forecast_col = 'Adj. Close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.1*len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
 
-forcast_col = 'Adj. Close'
+df.dropna(inplace=True)
+print(df)
