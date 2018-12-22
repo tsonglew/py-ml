b1a3574f7f6aae109094ff7c77233d86c6e2716e
kasheemlew <417879751@qq.com>
2018-12-22 12:20:28 +0800

    dogs-vs-cats convnet virtualization

---
+++ kaggle/dogs-vs-cats/virtualization/filter.py
@@ -0,0 +1,33 @@
+import numpy as np
+from keras import backend as K
+from keras.applications import VGG16
+
+model = VGG16(weights='imagenet', include_top=False)
+
+layer_name = 'block3_conv1'
+filter_index = 0
+
+layer_output = model.get_layer(layer_name).output
+loss = K.mean(layer_output[:, :, :, filter_index])
+grads = K.gradients(loss, model.input)[0]
+
+grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+iterate = K.function([model, input], [loss, grads])
+
+loss_value, grads_value = iterate([np.zeros((1, 150, 150, 3))])
+
+input_img_data = np.random.random((1, 150, 150, 3)) * 20 + 128.
+step = 1.
+for i in range(40):
+    loss_value, grads_value = iterate([input_img_data])
+    input_img_data += grads_value * step
+
+def deprocess_image(x):
+    x -= x.mean()
+    x /= (x.std() + 1e-5)
+    x *= 0.5
+    x = np.clip(x, 0, 1)
+
+    x *= 255
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
